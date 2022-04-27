c443b9ef6dc7ab5301b0ed56d289ce2b7488464d
Nara <narawit.tub@outlook.co.th>
2022-04-27 21:21:58 +0700

    :sparkles: (API) feat: add Get a list of nationalPark endpoint

---
+++ ParkyAPIs/Controllers/NationalParkController.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ParkyAPIs.Mapper;
+using ParkyAPIs.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPIs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NationalParkController : ControllerBase
+    {
+        private readonly INationalParkRepository _npRepository;
+        private readonly IMapper _mapper;
+
+        public NationalParkController(INationalParkRepository npRepo, IMapper mapper)
+        {
+            _npRepository = npRepo;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult GetNationalParks()
+        {
+            var objList = _npRepository.GetNationalParks();
+
+            var objDto = new List<NationalParkDto>();
+
+            foreach (var obj in objList)
+            {
+                objDto.Add(_mapper.Map<NationalParkDto>(obj));
+            }
+
+            return Ok(objList);
+        }
+    }
+}
